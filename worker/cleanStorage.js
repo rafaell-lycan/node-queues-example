4b56983b42b4c1d532575529b4ef95f746acdd3e
Rafaell Lycan <sonny.webdsg@gmail.com>
2019-05-14 14:18:24 +0200

    Add cleanStorage worker to keep everything clean every 24h ✨

---
+++ worker/cleanStorage.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const logger = require('../util/logger');
+const { createQueue, addListeners } = require('../util/queueManager');
+
+const { QUEUE_NAME, REDIS_URL } = process.env;
+
+// Create a Queue
+const queue = createQueue('housekeeper', REDIS_URL);
+addListeners(queue, logger);
+
+queue.process(async () => {
+  logger.info(`Cleaning storage for queue ${QUEUE_NAME}...`);
+  try {
+    const dirtyQueue = createQueue(QUEUE_NAME, REDIS_URL);
+    await dirtyQueue.clean(1000);
+    return Promise.resolve();
+  } catch (error) {
+    return Promise.reject();
+  }
+});
+
+queue.add(null, { repeat: { cron: '0 0 * * *' } });
